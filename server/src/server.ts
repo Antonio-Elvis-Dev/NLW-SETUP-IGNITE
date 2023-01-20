9fa77d7510af1f43d4138755ecf9c84094a61c2e
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-01-20 16:05:24 -0300

    bud de db solucionado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,15 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from '@fastify/cors'
+import { appRoutes } from './routes';
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 
 app.register(cors,{
-    origin:['http://localhost:3000']
+    origin:['http://localhost:3000'] // TODO segurança - pode apagar quando quiser
 })
 
-app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes)
 
 app
   .listen({
