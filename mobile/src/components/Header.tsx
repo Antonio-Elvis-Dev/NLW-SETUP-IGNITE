fefef837667472febeb9e1d14b7ac7b82d8847ee
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-01-26 16:34:24 -0300

    upgrade in layout mobile

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,6 +1,6 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import Logo from "../assets/logo.svg";
-import colors from 'tailwindcss/colors'
+import colors from "tailwindcss/colors";
 
 import { Feather } from "@expo/vector-icons";
 
@@ -8,10 +8,10 @@ export function Header() {
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
-      <Feather 
-      name="plus"
-      color={colors.violet[500]}
-      size={20} />
+      <TouchableOpacity className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center">
+        <Feather name="plus" color={colors.violet[500]} size={20} />
+        <Text className=" text-white ml-3 font-semibold text-base">Novo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,12 +1,51 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { Header } from "../components/Header";
+import { DAY_SIZE, HabitDay } from "../components/HabirDay";
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-dates-from-year-start";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimunSummaryDateSize = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDateSize - datesFromYearStart.length;
 
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16 ">
       <Header />
-      <Text className="text-white">Home</Text>
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, i) => (
+          <Text
+            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            key={`${weekDay} - ${i}`}
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({
+              length: amountOfDaysToFill,
+            }).map((_, i) => (
+              <View
+                key={i}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40  "
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
