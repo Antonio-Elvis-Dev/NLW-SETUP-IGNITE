fb5ef0da5406d6fbb7d6dadc38a4aa9784b94652
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-01-30 17:04:38 -0300

    upgrade in file mobile

--- mobile/src/components/CheckBox.tsx
+++ mobile/src/components/CheckBox.tsx
@@ -1,15 +1,21 @@
-import { TouchableOpacity, View, Text } from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  TouchableOpacityProps,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 
-interface Props {
+
+interface Props extends TouchableOpacityProps {
   title: string;
   checked?: boolean;
 }
 
-export function CheckBox({title, checked = false }: Props) {
+export function CheckBox({ title, checked = false, ...rest  }: Props) {
   return (
-    <TouchableOpacity className="flex-row mb-2 items-center">
+    <TouchableOpacity className="flex-row mb-2 items-center" {...rest}>
       {checked ? (
         <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
           <Feather name="check" size={20} color={Colors.white} />
@@ -18,7 +24,7 @@ export function CheckBox({title, checked = false }: Props) {
         <View className="h-8 w-8 bg-zinc-800 rounded-lg"></View>
       )}
 
-      <Text  className="text-white text-base ml-3">{title}</Text>
+      <Text className="text-white text-base ml-3">{title}</Text>
     </TouchableOpacity>
   );
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Header } from "../components/Header";
 import { DAY_SIZE, HabitDay } from "../components/HabirDay";
 import { generateRangeDatesFromYearStart } from "../utils/generate-range-dates-from-year-start";
+import { useNavigation } from "@react-navigation/native";
 
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
@@ -12,6 +13,8 @@ const minimunSummaryDateSize = 18 * 5;
 const amountOfDaysToFill = minimunSummaryDateSize - datesFromYearStart.length;
 
 export function Home() {
+
+  const {navigate} = useNavigation()
   return (
     <View className="flex-1 bg-background px-8 pt-16 ">
       <Header />
@@ -32,19 +35,27 @@ export function Home() {
         contentContainerStyle={{ paddingBottom: 100 }}
       >
         <View className="flex-row flex-wrap">
+
           {datesFromYearStart.map((date) => (
             <HabitDay key={date.toISOString()} />
           ))}
+
           {amountOfDaysToFill > 0 &&
+
             Array.from({
               length: amountOfDaysToFill,
             }).map((_, i) => (
+
               <View
                 key={i}
                 className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40  "
                 style={{ width: DAY_SIZE, height: DAY_SIZE }}
-              />
+
+                />
+
             ))}
+
+
         </View>
       </ScrollView>
     </View>
