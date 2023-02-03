e394589a83cb97c1391a5c01d2c642df0f93c449
Antônio Elvis P <githubantonioelvis@gmail.com>
2023-02-03 07:10:46 -0300

    update in NewHabitForm

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,24 +1,50 @@
 import { Check } from "phosphor-react";
+import * as CheckBox from "@radix-ui/react-checkbox";
+
+const avaliableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export function NewHabitForm() {
   return (
     <form className="w-full flex flex-col mt-6">
-      <label htmlFor="title" 
-      className="font-bold leading-tight"
-      >Qual seu comprometimento?</label>
+      <label htmlFor="title" className="font-bold leading-tight">
+        Qual seu comprometimento?
+      </label>
       <input
         type="text"
         id="title"
         placeholder="ex.: Execícios, dormir cedo... "
-      className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
       />
 
-      <label htmlFor=""
-      className="font-bold leading-tight mt-4"
-      
-      >Qual a recorrência?</label>
-
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center gap-3 font-bold bg-green-600 justify-center hover:bg-green-500">
+      <label htmlFor="" className="font-bold leading-tight mt-4">
+        Qual a recorrência?
+      </label>
+      <div className="flex flex-col gap-2 mt-3">
+        {avaliableWeekDays.map((weekDay) => {
+          return (
+            <CheckBox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <CheckBox.Indicator>
+                  <Check size={20} className="text-white" />
+                </CheckBox.Indicator>
+              </div>
+              <span className=" text-white leading-tight ">{weekDay}</span>
+            </CheckBox.Root>
+          );
+        })}
+      </div>
+      <button
+        type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center gap-3 font-bold bg-green-600 justify-center hover:bg-green-500"
+      >
         <Check size={20} weight="bold" />
         Confirmar
       </button>
